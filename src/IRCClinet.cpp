9d8ee4700f62275c5b329baabefe59bed319524a
C4e10VeK <tank.t3445@gmail.com>
2022-01-28 14:18:40 +1000

    add winsock

--- src/IRCClinet.cpp
+++ src/IRCClinet.cpp
@@ -2,19 +2,29 @@
 
 #include <iostream>
 #include <sstream>
-#include <unistd.h>
 
+#ifdef _WIN32
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#else
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#endif
 
 IRCClient::IRCClient() : m_socket(-1), m_connected(false) { }
 
 IRCClient::~IRCClient()
 {
 	disconnect();
+#ifdef _WIN32
+	closesocket(m_socket);
+	WSACleanup();
+#else
 	close(m_socket);
+#endif
 }
 
 bool IRCClient::isConnected() const
@@ -24,10 +34,22 @@ bool IRCClient::isConnected() const
 
 void IRCClient::connect(const char *host, const char *port)
 {
+	int err;
+
+#ifdef _WIN32
+	WSADATA wsaData;
+	err = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (err != 0)
+	{
+		std::cerr << "WSAStartup failed. Error = " << err << std::endl;
+		return;
+	}
+#endif
+
 	addrinfo addr{}, *addrList;
 	addr.ai_family = AF_UNSPEC;
 	addr.ai_socktype = SOCK_STREAM;
-	int err = getaddrinfo(host, port, &addr, &addrList);
+	err = getaddrinfo(host, port, &addr, &addrList);
 	if (err < 0)
 	{
 		std::cerr << "Error while connecting to: " << host << ":" << port << std::endl;
@@ -49,6 +71,7 @@ void IRCClient::connect(const char *host, const char *port)
 		return;
 	}
 	m_connected = true;
+	freeaddrinfo(addrList);
 }
 
 void IRCClient::send(const std::string &msg)
